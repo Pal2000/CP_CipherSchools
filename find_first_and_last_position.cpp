f11d4b83ea43387c4af7de19e69302bb490814f1
Pallavi <56464656+Pal2000@users.noreply.github.com>
2021-02-13 19:34:25 +0530

    Initial commit

---
+++ find_first_and_last_position.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+
+int findFirstIndex(const vector<int> &a, int n, int x)
+{
+    int l=0, h=n-1, m, index=-1;
+    while(l<=h){
+        m=(h+l)/2;
+        if(a[m]>=x){
+            if(a[m]==x) index=m;
+            h=m-1;
+        }
+        else l=m+1;
+    }
+    return index;
+}
+int findLastIndex(const vector<int> &a, int n, int x){
+    int l=0, h=n-1, m, index=-1;
+    while(l<=h){
+        m=(h+l)/2;
+        if(a[m]<=x){
+            if(a[m]==x) index=m;
+            l=m+1;
+        }
+        else h=m-1;
+    }
+    return index;
+}
+ 
+
+int32_t main()
+{
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++)
+    cin>>a[i];
+    int x;
+    cin>>x;
+    int f=findFirstIndex(a, n, x);
+    int l=findLastIndex(a, n, x);
+    cout<<f<<" "<<l;
+    return 0;
+}
