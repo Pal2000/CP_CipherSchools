d0b21e22e78a75b090efcf7a5283a453dded1ef2
Pallavi <56464656+Pal2000@users.noreply.github.com>
2021-02-20 23:00:49 +0530

    Update mergeTrees.cpp

--- Day5/mergeTrees.cpp
+++ Day5/mergeTrees.cpp
@@ -25,7 +25,7 @@ Node* mergeTrees(Node* t1, Node* t2) {
         if(t1 && !t2){
             return t1;
         }
-        t1->val=t1->val+t2->val;
+        t1->data=t1->data+t2->data;
         t1->left=mergeTrees(t1->left, t2->left);
         t1->right=mergeTrees(t1->right, t2->right);
         return t1;
