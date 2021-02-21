59faae6949d485e14faa4ab9da44afdffc7a9a74
Pallavi <56464656+Pal2000@users.noreply.github.com>
2021-02-21 23:54:24 +0530

    Create kthSmallest.cpp

---
+++ Day6/kthSmallest.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int kthSmallest(int arr[], int n, int k) {
+    
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for(int i=0;i<n;i++)
+    pq.push(arr[i]);
+    
+    while(!pq.empty() && k>1){
+        pq.pop();
+        k--;
+    }
+    return pq.top();
+}
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+	for(int i = 0;i< n; ++i)
+	{
+		cin >> a[i];
+	}
+	cout<<kthSmallest(a, n);
+    return 0;
+}
