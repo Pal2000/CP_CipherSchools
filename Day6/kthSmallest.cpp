2f0f17c1f466edda50d24e16740c001127ec0205
Pallavi <56464656+Pal2000@users.noreply.github.com>
2021-02-22 01:34:54 +0530

    Update kthSmallest.cpp

--- Day6/kthSmallest.cpp
+++ Day6/kthSmallest.cpp
@@ -20,10 +20,8 @@ int main()
     int n;
     cin>>n;
     int a[n];
-	for(int i = 0;i< n; ++i)
-	{
-		cin >> a[i];
-	}
-	cout<<kthSmallest(a, n);
+    for(int i = 0;i< n; ++i)
+    cin>>a[i];
+    cout<<kthSmallest(a, k);
     return 0;
 }
